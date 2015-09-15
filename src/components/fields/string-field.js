61b29b72f405d74ad0b2d0c93dc3967c990d3f92
Hans Fjallemark <hans.fjallemark@cignium.com>
2015-09-15 14:49:10 +0200

    Add support for textareas.

--- src/components/fields/string-field.js
+++ src/components/fields/string-field.js
@@ -11,21 +11,36 @@ export default class StringField extends Field {
       return super.render()
     }
 
-    return (
-      <select
-        className={`ct-field ct-select-field`}
-        id={this.props.property.id}
-        onBlur={(e) => this.onBlur(e)}
-        onChange={(e) => this.onChange(e)}
-        value={this.state.value}>
-        {this.props.property.options.map((option) => {
-          return (
-            <option key={option.value} value={option.value}>
-              {option.title}
-            </option>
-          )
-        })}
-      </select>
-    )
+    if (this.props.property.display === 'textarea') {
+      return (
+        <textarea
+          className={`ct-field ct-${this.type}-field`}
+          id={this.props.property.id}
+          onBlur={(e) => this.onBlur(e)}
+          onChange={(e) => this.onChange(e)}
+          value={this.state.value} />
+      )
+    }
+
+    if (this.props.property.display === 'select') {
+      return (
+        <select
+          className={`ct-field ct-select-field`}
+          id={this.props.property.id}
+          onBlur={(e) => this.onBlur(e)}
+          onChange={(e) => this.onChange(e)}
+          value={this.state.value}>
+          {this.props.property.options.map((option) => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.title}
+              </option>
+            )
+          })}
+        </select>
+      )
+    }
+
+    return <div>Unsupported display type</div>
   }
 }
