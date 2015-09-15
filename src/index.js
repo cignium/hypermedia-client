bae60a911e891b1f1284d63c566ff454ea201590
Hans Fjallemark <hans.fjallemark@cignium.com>
2015-09-15 16:21:54 +0200

    chore: add main file in package

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './app'
+import store from './store'
+import { navigate } from './actions'
+
+let initialized = false
+
+export default {
+  init(element, resource) {
+    if (!initialized) {
+      if (typeof element === 'string') {
+        element = document.getElementById(element)
+      }
+
+      ReactDOM.render(
+        <Provider store={store}>
+          <App initialHref={resource} />
+        </Provider>,
+        element
+      )
+
+      initialized = true
+    }
+    else {
+      store.dispatch(navigate(resource))
+    }
+  },
+}
