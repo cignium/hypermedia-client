cab8c3b7540a06e42a877ef1d45230937e723ee7
Hans Fjallemark <hans.fjallemark@cignium.com>
2015-10-02 19:19:16 +0200

    Add support for updating checkbox list values.

--- src/components/elements/inputs/checkbox-list.js
+++ src/components/elements/inputs/checkbox-list.js
@@ -1,29 +1,42 @@
 import React from 'react'
 import Input from './input'
 
+function isChecked(values, option) {
+  for (let value of values || []) {
+    if (option.value === value) {
+      return true
+    }
+  }
+}
+
 export default class CheckboxList extends Input {
-  onChange(value) {
-    this.setState({ value: [value] })
-    this.onUpdate([value])
+  onChange(e, value) {
+    let values = [...this.state.value || []]
+
+    if (e.target.checked) {
+      values.push(value)
+    }
+    else {
+      values.splice(values.indexOf(value), 1)
+    }
+
+    if (!values.length) {
+      values = null
+    }
+
+    this.setState({ value: values })
+    this.onUpdate(values)
   }
 
   render() {
     return (
       <div className={'ct-input ct-checkbox-list'}>
         {this.props.property.options.map((option) => {
-          let checked = false
-
-          for (let item in this.state.value || []) {
-            if (item.value === option.value) {
-              checked = true
-            }
-          }
-
           return (
             <div key={option.value}>
               <input
-                checked={checked}
-                onChange={(e) => this.onChange([option.value])}
+                checked={isChecked(this.state.value, option)}
+                onChange={(e) => this.onChange(e, option.value)}
                 name={this.props.property.id}
                 type='checkbox'
                 value={option.value} />
