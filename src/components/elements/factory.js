77ce5ff3416cb3c4d9a62906e1eae9c832366f83
Hans Fjallemark <hans.fjallemark@cignium.com>
2015-10-03 13:13:48 +0200

    refactor: section and element factories

---
+++ src/components/elements/factory.js
@@ -0,0 +1,38 @@
+import CheckboxList from './inputs/checkbox-list'
+import DatePicker from './inputs/date-picker'
+import DropdownList from './inputs/dropdown-list'
+import Link from './link'
+import List from './list'
+import MultilineTextInput from './inputs/multiline-text-input'
+import NumberInput from './inputs/number-input'
+import RadioButtonList from './inputs/radio-button-list'
+import Section from './section'
+import Table from './table'
+import TextInput from './inputs/text-input'
+
+function isTable(property) {
+  return property.items[0] && property.items[0].properties
+}
+
+export default (property) => {
+  switch (property.type) {
+    case 'array': return isTable(property) ? Table : List
+    case 'date': return DatePicker
+    case 'number': return NumberInput
+    case 'object': return Section
+    case 'string':
+      if (property.links.navigate) {
+        return Link
+      }
+
+      switch (property.display) {
+        case 'checkbox': return CheckboxList
+        case 'radio': return RadioButtonList
+        case 'select': return DropdownList
+        case 'textarea': return MultilineTextInput
+        default: return TextInput
+      }
+  }
+
+  throw Error(`Unsupported input type '${property.type}'`)
+}
--- src/components/elements/section.js
+++ src/components/elements/section.js
@@ -1,29 +1,11 @@
-import React, { PropTypes } from 'react'
-import Link from './link'
-import List from './list'
-import Table from './table'
-import inputFactory from './inputs/factory'
-
-function isTable(property) {
-  return property.items[0] && property.items[0].properties
-}
+import React from 'react'
+import factory from './factory'
 
 export default function Section({property, update}) {
   return (
     <div className='ct-section'>
       {property.properties.map(property => {
-        let Element = null
-
-        switch (property.type) {
-          case 'array':
-            Element = isTable(property) ? Table : List
-            break
-          case 'object':
-            Element = Section
-            break
-          default:
-            Element = property.links.navigate ? Link : inputFactory(property)
-        }
+        let Element = factory(property)
 
         return (
           <div className='ct-element' key={property.id}>
