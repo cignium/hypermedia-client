d786d37d1ca559be0d2c013ff3d4fdd77743440d
Hans Fjallemark <hans.fjallemark@cignium.com>
2015-10-15 14:41:43 +0200

    fix: include credentials in requests and add accept json header

--- src/api/http.js
+++ src/api/http.js
@@ -1,6 +1,8 @@
 export async function request(method, href, data) {
   const request = {
     body: data && JSON.stringify(data),
+    credentials: 'include',
+    headers: new Headers({ Accept: 'application/json' }),
     method,
   }
 
