0a2ea8310f0e8fb127f4c678644b3399637177c9
Hans Fjallemark <hans.fjallemark@cignium.com>
2015-11-02 16:34:09 +0100

    chore: add provide plugin i production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,6 +21,9 @@ export default {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
+    new webpack.ProvidePlugin({
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    }),
     new webpack.optimize.UglifyJsPlugin({
       comments: false,
       compress: {
