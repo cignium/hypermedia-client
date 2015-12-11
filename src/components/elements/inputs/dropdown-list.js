83305c9d112a4af34f05e553b9f2b3f75f0d3605
nranas <niklas.ranas@cignium.com>
2015-12-11 15:24:16 +0100

    fix: wrap select in div so that tooltip can be shown

--- src/components/elements/inputs/dropdown-list.js
+++ src/components/elements/inputs/dropdown-list.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import Select from 'react-select'
 
-export default ({ className, onCommit, property, value }) => {
+export default ({ className, errors, onCommit, property, value }) => {
   function getValue(option) {
     return option ? property.isArray ? option.map(option => option.value) :
       option.value : null
   }
 
   return (
-    <Select
-      className={`${className} ct-dropdown-list`}
-      disabled={property.disabled}
-      id={property.id}
-      multi={property.isArray}
-      onChange={option => onCommit(getValue(option))}
-      options={property.options.map(({ title, value }) => {
-        return { label: title, value }
-      })}
-      title={property.title}
-      value={value} />
+    <div data-tip={errors}>
+      <Select
+        className={`${className} ct-dropdown-list`}
+        disabled={property.disabled}
+        id={property.id}
+        multi={property.isArray}
+        onChange={option => onCommit(getValue(option))}
+        options={property.options.map(({ title, value }) => {
+          return { label: title, value }
+        })}
+        title={property.title}
+        value={value} />
+    </div>
   )
 }
