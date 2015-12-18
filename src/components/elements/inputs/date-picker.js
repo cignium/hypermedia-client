c4d66c54428ddbf5da3d1be10ee5e1aeb589a428
nranas <niklas.ranas@cignium.com>
2015-12-18 10:03:00 +0100

    fix: format date according to new api

--- src/components/elements/inputs/date-picker.js
+++ src/components/elements/inputs/date-picker.js
@@ -3,7 +3,7 @@ import DatePicker from 'react-datepicker'
 import moment from 'moment'
 import styles from './input-styles'
 
-function getISODate(date) {
+function getDate(date) {
   return date.add(date.utcOffset(), 'm').toISOString().split('T')[0]
 }
 
@@ -13,7 +13,7 @@ export default ({ className, errors, onCommit, property, value }) => (
       className={`${className} ${styles.input} ct-date-picker`}
       disabled={property.disabled}
       id={property.id}
-      onChange={date => onCommit(getISODate(date))}
+      onChange={date => onCommit(getDate(date))}
       selected={value ? moment(value) : value}
       title={property.title} />
   </div>
