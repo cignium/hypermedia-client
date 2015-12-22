d24d95e2e97fd0c7be8406939f20c3936cb8c6ef
nranas <niklas.ranas@cignium.com>
2015-12-22 13:39:25 +0100

    chore: Parse all attributes into options

--- src/index.js
+++ src/index.js
@@ -2,22 +2,41 @@ import { render } from 'react-dom'
 import { navigate } from './api'
 import App from './app'
 
-function init(element) {
+function init(element, options) {
   if (typeof element === 'string') {
     element = document.getElementById(element)
   }
 
-  const useDefaultStyling = element
-    .getAttribute('data-disable-default-styling') == null
+  render(<App {...options} />, element)
+}
 
-  render(<App defaultStyling={useDefaultStyling} />, element)
+function getOptions(element) {
+  const options = {}
+
+  Array.prototype.slice.call(element.attributes)
+    .filter(attr => attr.name.startsWith('data-'))
+    .map(attr => { options[getName(attr)] = getValue(attr) })
+
+  return options
+}
+
+function getName(attribute) {
+  return attribute.name
+    .slice(5)
+    .replace(/-([a-z])/g, match => {
+      return match[1].toUpperCase()
+    })
+}
+
+function getValue(attribute) {
+  return attribute.value == '' ? true : attribute.value
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const element = document.querySelector('[data-endpoint]')
 
   if (element) {
-    init(element)
+    init(element, getOptions(element))
 
     const url = element.getAttribute('data-endpoint')
 
