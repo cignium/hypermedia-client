3e7c2715b98ecde94e510c9ba7e91f4ed9439926
Edgar Cordova <edgar.cordova@cignium.com>
2016-01-04 13:33:38 -0500

    fix: Add style to required radio button group

--- src/components/elements/inputs/boolean-list.js
+++ src/components/elements/inputs/boolean-list.js
@@ -1,3 +1,5 @@
+import styles from './input.css'
+
 export default ({ className, onCommit, property, value }) => {
   function getValue(checked, newValue) {
     if (!property.isArray) {
@@ -29,7 +31,7 @@ export default ({ className, onCommit, property, value }) => {
   }
 
   return (
-    <div className={`${className} ct-${property.display}-list`}>
+    <div className={`${className} ${styles.input} ct-${property.display}-list`}>
       {property.options.map(option => {
         return (
           <div key={option.value}>
