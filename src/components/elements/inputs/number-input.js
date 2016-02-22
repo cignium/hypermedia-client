67d6b53a5b6a7dcdf6aa1f4eff27c8ea6cf19c02
Hans Fjallemark <hans.fjallemark@cignium.com>
2016-02-22 14:27:17 +0100

    fix: post null instead of empty string when emptying number fields. fixes #51

--- src/components/elements/inputs/number-input.js
+++ src/components/elements/inputs/number-input.js
@@ -7,7 +7,7 @@ export default ({ className, errors, onCommit, onUpdate, property, value }) => (
     disabled={property.disabled}
     id={property.name}
     onBlur={() => onCommit()}
-    onChange={e => onUpdate(e.target.value)}
+    onChange={e => onUpdate(e.target.value == '' ? null : parseFloat(e.target.value))}
     title={property.title}
     type='number'
     value={value} />
