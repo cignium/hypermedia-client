cf0ac52d3cbc4247ce391b8e16f6114b66b5c07c
nranas <niklas.ranas@cignium.com>
2016-03-14 13:57:40 +0100

    chore: Corrected whitespaces and newlines. Shame on me.

--- src/components/error-message.js
+++ src/components/error-message.js
@@ -2,16 +2,30 @@ export default ({ requestError, documentErrors }) => {
   if (requestError) {
     return (
       <div className='ct-error-container'>
-        { requestError.message }
-      </div>)
+        {requestError.message}
+      </div>
+    )
   }
 
   if (documentErrors && documentErrors.length) {
     return (
       <div className='ct-error-container'>
         <ul className='ct-error-list'>
-          { documentErrors.map((error, index) =>
-            <li className='ct-error-list-message' key={index}>{error}</li>)}
+          {documentErrors.map((error, index) =>
+            <li className='ct-error-list-message' key={index}>{error}</li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+
+  if (documentErrors && documentErrors.length) {
+    return (
+      <div className='ct-error-container'>
+        <ul className='ct-error-list'>
+          {documentErrors.map((error, index) =>
+            <li className='ct-error-list-message' key={index}>{error}</li>
+          )}
         </ul>
       </div>
     )
