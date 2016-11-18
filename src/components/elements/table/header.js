a450cb4d604b4b0d71c0702611ee7669b3632acc
robin.carlander <Robin.Carlander@cignium.com>
2016-11-18 12:50:00 +0100

    feat: Sorting tables

--- src/components/elements/table/header.js
+++ src/components/elements/table/header.js
@@ -1,21 +1,53 @@
+import { Component } from 'react'
 import cx from 'classnames'
 
-export default ({ api, property }) => {
-  if (!property.columns) {
-    return <thead/>
+export default class Header extends Component {
+  constructor(props) {
+    super()
+
+    this.state = {
+      sortedColumn: null,
+      direction: null,
+    }
   }
-  return (
-    <thead>
-      <tr className='ct-table-header-row'>
-        {property.columns.map((column, rowIndex) => {
-          return (
-            <th key={column.id} className='ct-table-header'>
-              {column.title}
-            </th>
-          )
-        })
-        }
-      </tr>
-    </thead>
-  )
+
+  render() {
+    if (!this.props.columns) {
+      return <thead />
+    }
+    return (
+      <thead>
+        <tr className='ct-table-header-row'>
+          {this.props.columns.map((column, columnIndex) => {
+            return (
+              <th
+                onClick={e => this.onSort(e, columnIndex) }
+                key={column.id}
+                className='ct-table-header'>
+                <a href='#'
+                  className={'ct-table-header-sort-link'}>
+                  {column.title}</a>
+                <span className={cx('ct-table-header-sort-direction', {
+                  'ct-table-header-sort-ascending': this.state.sortedColumn == columnIndex &&
+                    !this.state.direction,
+                  'ct-table-header-sort-descending': this.state.sortedColumn == columnIndex &&
+                    this.state.direction})}>
+                </span>
+              </th>
+            )
+          })
+          }
+        </tr>
+      </thead>
+    )
+  }
+
+  onSort(e, columnIndex) {
+    e.preventDefault()
+    this.setState({
+      sortedColumn: columnIndex,
+      direction: this.state.sortedColumn == columnIndex ? !this.state.direction : null,
+    })
+    this.props.sortBy(columnIndex)
+  }
 }
--- src/components/elements/table/index.js
+++ src/components/elements/table/index.js
@@ -1,14 +1,67 @@
+import { Component } from 'react'
 import factory from '../factory'
-import cx from 'classnames'
 import Header from './header'
 import Body from './body'
 
-export default ({ api, property }) => (
-  <div className='ct-table-container'>
-    <label className='ct-element-label ct-table-label'>{property.title}</label>
-    <table className='ct-table'>
-      <Header api={api} property={property} />
-      <Body api={api} property={property} />
-    </table>
-  </div>
-)
+export default class Table extends Component {
+  constructor(props) {
+    super()
+
+    this.state = {
+      sortedRows: props.property.rows.slice(),
+      currentColumnIndex: null,
+    }
+  }
+
+  render() {
+    return (
+      <div className='ct-table-container'>
+        <label className='ct-element-label ct-table-label'>{this.props.property.title}</label>
+        <table className='ct-table'>
+          <Header
+            api={this.props.api}
+            columns={this.props.property.columns}
+            sortBy={index => this.sortBy(index)} />
+          <Body rows={this.state.sortedRows} />
+        </table>
+      </div>)
+  }
+
+  sortBy(columnIndex) {
+    const rowsToSort = this.state.sortedRows
+    let sorted = rowsToSort.sort((row1, row2) => {
+      if (this.getRowValue(row1, columnIndex) > this.getRowValue(row2, columnIndex)) return 1
+      if (this.getRowValue(row1, columnIndex) < this.getRowValue(row2, columnIndex)) return -1
+      return 0
+    })
+    if (this.state.currentColumnIndex == columnIndex) {
+      sorted = sorted.reverse()
+    }
+    this.setState({
+      sortedRows: sorted,
+      currentColumnIndex: this.state.currentColumnIndex == columnIndex ? null : columnIndex,
+    })
+    return false
+  }
+
+  getRowValue(row, columnIndex) {
+    if (row[columnIndex].type == 'html') {
+      const doc = document.implementation.createHTMLDocument('')
+      doc.documentElement.innerHTML = row[columnIndex].content
+
+      return doc.getElementsByTagName('body')[0].innerText
+    }
+    if (row[columnIndex].type == 'number') {
+      return parseFloat(row[columnIndex].value)
+    }
+    if (row[columnIndex].type == 'date' || row[columnIndex].type == 'datetime') {
+      return new Date(row[columnIndex].value)
+    }
+    if (row[columnIndex].type == 'boolean') {
+      return row[columnIndex].value.toLowerCase() === 'true'
+    }
+
+    return row[columnIndex].value
+  }
+}
+
