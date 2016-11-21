8cfcac0affd8409225bb9dd074a89a9bcef08a3c
robin.carlander <Robin.Carlander@cignium.com>
2016-11-21 09:37:12 +0100

    PR observation simplification of syntax

--- src/components/elements/table/header.js
+++ src/components/elements/table/header.js
@@ -8,28 +8,26 @@ export default ({ columns, sortedColumn, descending, sortBy }) => {
   return (
     <thead>
       <tr className='ct-table-header-row'>
-        {columns.map((column, columnIndex) => {
-          return (
-            <th
-              onClick={e => {
-                e.preventDefault()
-                sortBy(columnIndex)
-              }}
-              key={column.id}
-              className='ct-table-header'>
-              <a href='#'
-                className={'ct-table-header-sort-link'}>
-                {column.title}</a>
-              <span className={cx('ct-table-header-sort-direction', {
-                'ct-table-header-sort-ascending': sortedColumn == columnIndex &&
-                !descending,
-                'ct-table-header-sort-descending': sortedColumn == columnIndex &&
-                descending,
-              })}>
-              </span>
-            </th>
+        {columns.map((column, columnIndex) =>
+          <th
+            onClick={e => {
+              e.preventDefault()
+              sortBy(columnIndex)
+            }}
+            key={column.id}
+            className='ct-table-header'>
+            <a href='#'
+              className={'ct-table-header-sort-link'}>
+              {column.title}</a>
+            <span className={cx('ct-table-header-sort-direction', {
+              'ct-table-header-sort-ascending': sortedColumn == columnIndex &&
+              !descending,
+              'ct-table-header-sort-descending': sortedColumn == columnIndex &&
+              descending,
+            })}>
+            </span>
+          </th>
           )
-        })
         }
       </tr>
     </thead>
