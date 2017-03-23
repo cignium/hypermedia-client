62221617440159008cc988fae4db2bf4035b3b4a
robin.carlander <Robin.Carlander@cignium.com>
2017-03-23 15:42:38 +0100

    feat: improving date selection
    Adding date picker as an alternative display type. Picker also allow pasting in values in different formats (YYYY-MM-DD, MM/DD/YYYY, MM-DD-YYYY)

--- __tests__/date-picker-tests.js
+++ __tests__/date-picker-tests.js
@@ -1,13 +1,28 @@
 // __tests__/date-picker-tests.js
-jest.unmock('../src/components/elements/inputs/date/date-picker')
-jest.unmock('../src/components/elements/inputs/date/date-util')
+jest.unmock('../src/components/elements/inputs/date/datetime-picker')
+jest.unmock('Moment')
+jest.unmock('../node_modules/moment/moment.js')
+jest.unmock('../node_modules/react-widgets/lib/localizers/moment.js')
 
 import React from 'react'
 import TestUtils from 'react-addons-test-utils'
-import DatePicker from '../src/components/elements/inputs/date/date-picker'
+import DatePicker from '../src/components/elements/inputs/date/datetime-picker'
+import Moment from 'moment'
 
 describe('Basic date picker with value 2012-01-31', () => {
   let datePicker
+
+  function getComponent(which) {
+    if (which === 'Year') {
+      return datePicker.props.children.props.children[0]
+    }
+    if (which === 'Month') {
+      return datePicker.props.children.props.children[1]
+    }
+    if (which === 'Day') {
+      return datePicker.props.children.props.children[2]
+    }
+  }
 
   beforeEach(() => {
     const renderer = TestUtils.createRenderer()
@@ -18,17 +33,17 @@ describe('Basic date picker with value 2012-01-31', () => {
   })
 
   it('sets the year to 2012', () => {
-    const year = datePicker.props.children[0]
+    const year = getComponent('Year')
     expect(year.props.value.getUTCFullYear()).toEqual(2012)
   })
 
   it('sets the month to 0', () => {
-    const month = datePicker.props.children[1]
+    const month = getComponent('Month')
     expect(month.props.value.getUTCMonth()).toEqual(0)
   })
 
   it('sets the day to 31', () => {
-    const day = datePicker.props.children[2]
+    const day =  getComponent('Day')
     expect(day.props.value.getUTCDate()).toEqual(31)
   })
 })
--- src/components/elements/inputs/date/datetime-picker.js
+++ src/components/elements/inputs/date/datetime-picker.js
@@ -4,45 +4,73 @@ import Month from './month'
 import Day from './day'
 import Hour from './hour'
 import Minute from './minute'
+import DateTimePicker from 'react-widgets/lib/DateTimePicker'
+import Moment from 'moment'
+import momentLocalizer from 'react-widgets/lib/localizers/moment'
+import 'react-widgets/dist/css/react-widgets.css'
 
 function getDateString(date) {
-  return date && date.toISOString()
+  if (!date || !date.isValid()) {
+    return null
+  }
+  return date.toISOString()
 }
 
 export default ({ className, errors, onCommit, property, value }) => {
+  const formats = ['MM/DD/YYYY HH:mm A', 'MM/DD/YYYY H:mm A', 'MM-DD-YYYY HH:mm A',
+    'MM-DD-YYYY H:mm A', 'MM/DD/YYYY', 'MM-DD-YYYY','YYYYMMDD HH:mm', 'YYYY-MM-DD HH:mm']
   const date = value ? new Date(value) : null
+  momentLocalizer(Moment)
+
   function handleChange(date) {
     onCommit(getDateString(date))
   }
 
-  return (
-    <div data-tip={errors} className={cx(className, 'ct-datetime-picker')}>
+  function renderDropdowms() {
+    return (<span>
       <Year
         className={className}
-        onCommit={date => handleChange(date)}
+        onCommit={date => handleChange(Moment(date))}
         value={date}
         property={property} />
       <Month
         className={className}
-        onCommit={date => handleChange(date)}
+        onCommit={date => handleChange(Moment(date))}
         value={date}
         property={property} />
       <Day
         className={className}
-        onCommit={date => handleChange(date)}
+        onCommit={date => handleChange(Moment(date))}
         value={date}
         property={property} />
       <Hour
         className={className}
-        onCommit={date => handleChange(date)}
+        onCommit={date => handleChange(Moment(date))}
         value={date}
         property={property} />
-      <span className={cx(className, 'ct-time-separator') }>: </span>
+      <span className={cx(className, 'ct-time-separator')}>: </span>
       <Minute
         className={className}
-        onCommit={date => handleChange(date)}
+        onCommit={date => handleChange(Moment(date))}
         value={date}
         property={property} />
+    </span>)
+  }
+
+  function renderPicker() {
+    return (<DateTimePicker
+      disabled={property.disabled}
+      value={date}
+      parse={str => Moment(str, formats)}
+      className={cx(className, 'ct-datetime-picker')}
+      format='L LT'
+      onChange={date => handleChange(Moment(date))}
+      />)
+  }
+
+  return (
+    <div data-tip={errors} className={cx(className, 'ct-datetime-picker')}>
+      {property.display === 'select' ? renderDropdowms() : renderPicker()}
     </div>
   )
 }
