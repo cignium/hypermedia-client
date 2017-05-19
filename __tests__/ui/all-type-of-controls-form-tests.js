be2c383492c114b26d18d1afbb3a6541bc3b0ec5
Niklas Ranås <niklas.ranas@cignium.com>
2017-05-19 11:49:58 +0200

    chore: added ui tests for default values

--- __tests__/ui/all-type-of-controls-form-tests.js
+++ __tests__/ui/all-type-of-controls-form-tests.js
@@ -1,11 +1,31 @@
 const assert = require('assert')
 
-describe('All type of controls form', () => {
-  it('can load the form and find the header', () => {
+describe('Default values form', () => {
+  before(() => {
     browser.url('index.html')
-    const selector = '.ct-document-header-text'
-    browser.waitForExist(selector, 30000)
+    return browser.waitForExist('.ct-document-header-text', 30000)
+  })
 
-    assert.equal(browser.getText(selector), 'All type of controls form')
+  it('can load the form and find the header', () => {
+    assert.equal(browser.getText('.ct-document-header-text'), 'Default values')
+  })
+
+  it('checkbox default value is true', () => {
+    assert.equal(browser.isSelected('#checkbox'), true)
+  })
+
+  it('has a date element with correct default value', () => {
+    assert.equal(browser.getValue('.ct-date-picker .ct-month'), 4)
+    assert.equal(browser.getValue('.ct-date-picker .ct-day'), 19)
+    assert.equal(browser.getValue('.ct-date-picker .ct-year'), 1982)
+  })
+
+  it('has a date time element with correct default value', () => {
+    assert.equal(browser.getValue('.ct-datetime-picker .ct-month'), 4)
+    assert.equal(browser.getValue('.ct-datetime-picker .ct-day'), 20)
+    assert.equal(browser.getValue('.ct-datetime-picker .ct-year'), 1987)
+    assert.equal(browser.getValue('.ct-datetime-picker .ct-hour'), 1)
+    assert.equal(browser.getValue('.ct-datetime-picker .ct-minute'), 37)
+    assert.equal(browser.getText('.ct-datetime-picker .ct-am-pm'), 'PM')
   })
 })
