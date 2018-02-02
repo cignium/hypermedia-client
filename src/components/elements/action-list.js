9fc28908ba79f7ab08329a156d44af12b65c7434
Edgar Cordova <edgar.cordova@cignium.com>
2018-02-02 18:18:58 -0500

    Remove toggle button

--- src/components/elements/action-list.js
+++ src/components/elements/action-list.js
@@ -1,23 +1,14 @@
-import ToggleButton from './toggle-button'
-
 export default ({ api, config, links, requests }) => (
   <div className='ct-action-list'>
     {links.actions.map(action => {
-      return !config.actionListToggle ? (
+      return (
         <button
           className='ct-action'
           key={action.href}
+          disabled={config.actionListToggle && Object.keys(requests).length}
           onClick={() => api.executeAction(action.href, config)}>
           {action.title}
         </button>
-      )
-      : (
-        <ToggleButton 
-          api={api}
-          action={action}
-          config={config}
-          requests={requests}
-        />
       )
     })}
     {links.submit ? (
